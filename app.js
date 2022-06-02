eef9c7ecbd915005cecdbd3ba381e3f01dfcaa78
Shovna <shovnapanda01@gmail.com>
2022-06-03 01:10:16 +0530

    changes 1

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ app.get('/signup', (req, res) => {
 
 if (process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
-  //const path = require("path");
-  //app.get("*", (req,res) => {
-    //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  //})
+  const path = require("path");
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 }
 
 app.listen(PORT, () => {
